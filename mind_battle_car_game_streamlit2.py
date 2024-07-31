fb5576c2b4d4757c7359109798605c6c7724b985
riccardoboscariol <138816765+riccardoboscariol@users.noreply.github.com>
2024-07-31 15:54:46 +0200

    mind_battle_car_game_streamlit2.py

--- mind_battle_car_game_streamlit2.py
+++ mind_battle_car_game_streamlit2.py
@@ -161,7 +161,7 @@ def main():
         }
         .flag-image {
             position: absolute;
-            top: -77px;
+            top: -85px;
             width: 150px;
             left: 96%;  /* Sposta la bandierina a destra, cambia il valore per regolare */
         }
