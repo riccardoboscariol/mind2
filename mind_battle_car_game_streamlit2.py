7f07d2482b0786d3a2ded7286fd39688e678b881
riccardoboscariol <138816765+riccardoboscariol@users.noreply.github.com>
2024-08-06 20:58:43 +0200

    mind_battle_car_game_streamlit2.py

--- mind_battle_car_game_streamlit2.py
+++ mind_battle_car_game_streamlit2.py
@@ -154,8 +154,8 @@ def main():
         .stSlider > div > div > div > div > div {
             background: white; /* Color of the slider thumb */
             border-radius: 50%;
-            height: 14px;
-            width: 14px;
+            height: 2px;  /* Make slider thumb small */
+            width: 2px;  /* Make slider thumb small */
             position: relative;
             top: 0px; /* Correct slider thumb position */
         }
@@ -176,8 +176,8 @@ def main():
         }
         .number-image {
             position: absolute;
-            top: 37px;  /* Position for numbers, 1px lower */
-            left: calc(50% - 66px); /* Center numbers under cars */
+            top: 39px;  /* Position for numbers, 2px lower */
+            left: calc(50% - 68px); /* Center numbers under cars, 1px left */
             transform: translateX(-50%); /* Adjust to perfectly center */
             width: 120px;  /* Width of the number images slightly larger */
             z-index: 10;  /* Ensure numbers are below cars */
@@ -371,7 +371,7 @@ def main():
                 <img src="data:image/png;base64,{car_image_base64}" class="car-image" style="left:{st.session_state.car_pos / 10}%">
                 <!-- Red car number image -->
                 <img src="data:image/png;base64,{red_car_number_base64}" class="number-image {'show' if st.session_state.player_choice is not None else ''}" 
-                     style="left:calc({st.session_state.car_pos / 10}% + 6.7%); top: 37px; z-index: 10;">
+                     style="left:calc({st.session_state.car_pos / 10}% + 6.7%); top: 39px; z-index: 10;">
                 <input type="range" min="0" max="1000" value="{st.session_state.car_pos}" disabled>
                 <img src="data:image/png;base64,{flag_image_base64}" class="flag-image">
             </div>
@@ -386,7 +386,7 @@ def main():
                 <img src="data:image/png;base64,{car2_image_base64}" class="car-image" style="left:{st.session_state.car2_pos / 10}%">
                 <!-- Green car number image -->
                 <img src="data:image/png;base64,{green_car_number_base64}" class="number-image {'show' if st.session_state.player_choice is not None else ''}" 
-                     style="left:calc({st.session_state.car2_pos / 10}% + 6.7%); top: 37px; z-index: 10;">
+                     style="left:calc({st.session_state.car2_pos / 10}% + 6.7%); top: 39px; z-index: 10;">
                 <input type="range" min="0" max="1000" value="{st.session_state.car2_pos}" disabled>
                 <img src="data:image/png;base64,{flag_image_base64}" class="flag-image">
             </div>
