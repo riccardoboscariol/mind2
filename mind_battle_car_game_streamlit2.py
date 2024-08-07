f90e8668cb2a10b1300969d3c9b252d55354fd26
riccardoboscariol <138816765+riccardoboscariol@users.noreply.github.com>
2024-08-07 18:31:09 +0200

    mind_battle_car_game_streamlit2.py

--- mind_battle_car_game_streamlit2.py
+++ mind_battle_car_game_streamlit2.py
@@ -152,10 +152,10 @@ def main():
             background: #ccc; /* Color of the slider track */
         }
         .stSlider > div > div > div > div > div {
-            background: #fff; /* White color to make thumb invisible */
+            background: transparent; /* Make slider thumb invisible */
             border-radius: 50%;
-            height: 1px;  /* Size of the thumb */
-            width: 1px;  /* Size of the thumb */
+            height: 0px;  /* Reduce slider thumb height */
+            width: 0px;  /* Reduce slider thumb width */
             position: relative;
             top: 0px; /* Correct slider thumb position */
         }
@@ -190,12 +190,31 @@ def main():
             left: 93%;  /* Move flag 3px left */
         }
         .slider-container input[type=range] {
+            -webkit-appearance: none;
+            appearance: none;
             width: 100%;
             position: absolute;
             top: 138px;  /* Slider 22px higher */
         }
-        .slider-container input[type=range]:focus {
-            outline: none;
+        .slider-container input[type=range]::-webkit-slider-thumb {
+            -webkit-appearance: none;
+            appearance: none;
+            width: 0px;
+            height: 0px;
+            background: transparent;
+            cursor: pointer;
+        }
+        .slider-container input[type=range]::-moz-range-thumb {
+            width: 0px;
+            height: 0px;
+            background: transparent;
+            cursor: pointer;
+        }
+        .slider-container input[type=range]::-ms-thumb {
+            width: 0px;
+            height: 0px;
+            background: transparent;
+            cursor: pointer;
         }
         .stButton > button {
             display: inline-block;
@@ -563,3 +582,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
